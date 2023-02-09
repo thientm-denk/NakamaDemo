0af3d13ed549f5b930f7719bb49689a3cb332901
Denkhotieu <denkhotieu@gmail.com>
2023-02-09 17:07:27 +0700

    ping pong request

--- Assets/NakamaConnection.cs
+++ Assets/NakamaConnection.cs
@@ -24,6 +24,7 @@ public class NakamaConnection : MonoBehaviour
     private ISocket socket; // allow to send and recive message thought nakama sever 
     
     private string ticket; // save ticket when find match
+    private string matchId; // save id when finded match
     async void Start()
     {
         // create client for connection
@@ -39,11 +40,13 @@ public class NakamaConnection : MonoBehaviour
         
         // add unity action to matchmaking event  -> this method will run if socket get a matchmaking ticket from socket
         socket.ReceivedMatchmakerMatched += OnReciveMatchMakingMatched;
-        
+        socket.ReceivedMatchState += OnReciveMatchState;
         // 
         Debug.Log(session);
         Debug.Log(socket);
     }
+
+   
 
     public async void FindGame()
     {
@@ -53,17 +56,41 @@ public class NakamaConnection : MonoBehaviour
         ticket = matchMakingTicket.Ticket;
     }
 
+    public async void Ping()
+    {
+        Debug.Log("Pinging");
+
+        await socket.SendMatchStateAsync(matchId, 1, "", null);
+    }
+
 
     private async void OnReciveMatchMakingMatched(IMatchmakerMatched matchmakerMatched)
     {
         var match = await socket.JoinMatchAsync(matchmakerMatched);
-        
+        matchId = match.Id;
         // this session
         Debug.Log("Our session ID: " + match.Self.SessionId);
 
         foreach (var user in match.Presences) //  user in match
         {
             Debug.Log("Connected user session ID: " + user.SessionId);
+        }
+    }
+    
+    private async void OnReciveMatchState(IMatchState matchState)
+    {
+        if (matchState.OpCode == 1)
+        {
+            Debug.Log("Receive ping:");
+            Debug.Log("Sending pong");
+            await socket.SendMatchStateAsync(matchId, 2, "", new[] {matchState.UserPresence});
+        }
+        
+        if (matchState.OpCode == 2)
+        {
+            Debug.Log("Receive pong:");
+            Debug.Log("Sending ping");
+            await socket.SendMatchStateAsync(matchId, 1, "", new[] {matchState.UserPresence});
         }
     }
 }
