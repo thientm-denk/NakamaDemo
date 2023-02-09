33817626361e393af3cd1f858c21f406d76e3983
Denkhotieu <denkhotieu@gmail.com>
2023-02-09 10:55:01 +0700

    push origin project with Nakama Packed add

---
+++ Assets/NakamaConnection.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NakamaConnection : MonoBehaviour
+{
+    // http or https -> this demo using local sever -> http
+    private string scheme = "";
+    
+    
+    void Start()
+    {
+        
+    }
+
+   
+}
